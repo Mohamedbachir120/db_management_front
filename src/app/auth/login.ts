e0897adfbc44637e08ccc8bc12263f9afec521bc
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2022-12-28 07:10:14 +0100

    store auth info

--- src/app/auth/login.ts
+++ src/app/auth/login.ts
@@ -1,49 +1,59 @@
-import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-interface  Credential {
-    id:number,
-    name:string,
-    token:string
+interface Credential {
+    id: number,
+    name: string,
+    token: string
 }
- export  class loginParams{
-    constructor(email:string,password:string){
+export class loginParams {
+    constructor(email: string, password: string) {
         this.email = email;
         this.password = password;
     }
-    email:string;
-    password:string;
+    email: string;
+    password: string;
 
 }
 export const apiSlice = createApi({
-    reducerPath:'api',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'http://10.80.100.252:8000/api/auth'
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://192.168.0.127:8000/api/auth'
 
     }),
     endpoints: (builder) => ({
-       
-        login:builder.mutation<Credential,loginParams>({
-            query:(credentials) =>({
-                url:"/login",
-                method:'post',
-                body:{
-                    email:credentials.email,
-                    password:credentials.password
+
+        login: builder.mutation<Credential, loginParams>({
+            query: (credentials) => ({
+                url: "/login",
+                method: 'post',
+                body: {
+                    email: credentials.email,
+                    password: credentials.password
                 },
-                responseHandler:(response) => {
+                responseHandler: (response) => {
+                 
+                    response.json().then((e)=>{
+                        if(e.status == true){
+                            localStorage.setItem('token', e.token);
+                            localStorage.setItem('username', e.name);
+                            localStorage.setItem('id', e.id);
+
+                        }
+                      
+                    });
                     
-                    return response.text();
-                    
+                    return response.json();
+
 
                 }
 
             }),
 
         })
-       
+
     })
 
 })
 export const {
-useLoginMutation} = apiSlice;
+    useLoginMutation } = apiSlice;
 
